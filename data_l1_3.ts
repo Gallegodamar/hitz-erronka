049c321100d99c0166c4538d31d8d3caa4c64a85
Gallegodamar <30339413+Gallegodamar@users.noreply.github.com>
2026-02-11 07:40:44 +0100

    Update data_l1_3.ts

--- data_l1_3.ts
+++ data_l1_3.ts
@@ -19,5 +19,7 @@ export const LEVEL_1_PART_3: WordData[] = [
   {"id": "1-214", "hitza": "iskin egin", "sinonimoak": ["saihestu", "itzuri", "baztertu"]},
   {"id": "1-215", "hitza": "ezmezean egon", "sinonimoak": ["zalantzan egon"]},
   {"id": "1-216", "hitza": "inozo", "sinonimoak": ["tolesgabe", "bakun", "inozente"]},
-  {"id": "1-217", "hitza": "eskatu", "sinonimoak": ["galdegin", "galdatu"]}
+  {"id": "1-217", "hitza": "eskatu", "sinonimoak": ["galdegin", "galdatu"]},
+  {"id": "1-218","hitza":"eskeleto","sinonimoak":["hezurdura"]}
 ];
+
